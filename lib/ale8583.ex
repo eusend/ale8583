1faf7fe9678f8f69c256e0b058e304890f92d092
euge.mendoz <euge.mendoz@gmail.com>
2019-05-15 17:21:45 -0500

    Add support for missing mtis

--- lib/ale8583.ex
+++ lib/ale8583.ex
@@ -3,19 +3,23 @@ defmodule Ale8583 do
 
   @msgs [
     "0100",
-    "0190",
     "0110",
     "0120",
     "0130",
+    "0190",
     "0200",
     "0210",
     "0220",
     "0230",
+    "0320",
+    "0330",
     "0400",
     "0410",
     "0420",
     "0421",
     "0430",
+    "0500",
+    "0510",
     "0800",
     "0810",
     "0620",
@@ -37,7 +41,7 @@ defmodule Ale8583 do
     "9810",
     "9620"
   ]
- # @staticFields [:MMDDhhmmss, :hhmmss, :MMDD, :YYMM]
+  # @staticFields [:MMDDhhmmss, :hhmmss, :MMDD, :YYMM]
 
   @moduledoc """
   Documentation for Ale8583, a formater for ISO - 8583 financial transaction standar .
@@ -47,7 +51,13 @@ defmodule Ale8583 do
   {:iso ,.. {:ok, "OK"}}
 
   """
-  @spec new({any(), any()}) :: {:error, [], <<_::704>>} | {:error, [], {}, <<_::64, _::_*8>> | {:error, <<_::64, _::_*8>>}} | {:iso, [{:bp, <<_::128>>} | {:bs, <<_::128>>} | {:header_prosa, <<>>} | {:mti, any()}, ...], {[any()], [any()], :flag_bm_sec_no, any()}, {:ok, <<_::16>>}}
+
+  @spec new({any(), any()}) ::
+          {:error, [], <<_::704>>}
+          | {:error, [], {}, <<_::64, _::_*8>> | {:error, <<_::64, _::_*8>>}}
+          | {:iso,
+             [{:bp, <<_::128>>} | {:bs, <<_::128>>} | {:header_prosa, <<>>} | {:mti, any()}, ...],
+             {[any()], [any()], :flag_bm_sec_no, any()}, {:ok, <<_::16>>}}
   def new({mti, b}, headerProsa \\ "") do
     cond do
       is_bitstring(mti) && is_bitstring(b) ->
@@ -63,7 +73,6 @@ defmodule Ale8583 do
          "Error first mti should be bitstring type, second parameter should be list or bitstring ."}
     end
   end
-
 
   @doc """
   Function for validate if a field have in ISO.
@@ -89,9 +98,9 @@ defmodule Ale8583 do
     list_bit_map = Ale8583.BitMap.get_list_bit_map(str_bit_map)
     #  Logger.debug " inspect #{inspect list_bit_map}"
     if int_field in list_bit_map do
-        true
+      true
     else
-        false
+      false
     end
   end
 
@@ -151,7 +160,6 @@ defmodule Ale8583 do
         {key, value},
         {:iso, list_iso, {list_bit_map_p, list_bit_map_s, flag_bm_sec, list_iso_conf}, _}
       ) do
-
     {atom_key, key} =
       case is_atom(key) do
         true ->
@@ -172,13 +180,24 @@ defmodule Ale8583 do
       {_, map} ->
         case Ale8583.ValidISO.valid({key, value}, map) do
           {:ok, _} ->
-            sec_add_field(key, atom_key, value, list_bit_map_p, list_bit_map_s, list_iso, flag_bm_sec, list_iso_conf)
+            sec_add_field(
+              key,
+              atom_key,
+              value,
+              list_bit_map_p,
+              list_bit_map_s,
+              list_iso,
+              flag_bm_sec,
+              list_iso_conf
+            )
 
           {:error, msg_error} ->
             Logger.error(" :error #{msg_error}")
+
             {:iso, list_iso, {list_bit_map_p, list_bit_map_s, flag_bm_sec, list_iso_conf},
              {:error, msg_error}}
         end
+
       _ ->
         {:iso, list_iso, {list_bit_map_p, list_bit_map_s, flag_bm_sec, list_iso_conf},
          {:error, "Error field #{key} doesn't exist in configuration file."}}
@@ -288,7 +307,7 @@ defmodule Ale8583 do
         list_iso_conf
       )
 
-    #Logger.debug("[#{str_mti}|#{str_bp}|#{str_bs}|#{inspect(value)}]")
+    # Logger.debug("[#{str_mti}|#{str_bp}|#{str_bs}|#{inspect(value)}]")
 
     {_, map} = List.keyfind(list_iso_conf, :bp, 0)
 
@@ -309,7 +328,7 @@ defmodule Ale8583 do
            str_mti |> String.to_charlist() |> Ale8583.Convert.ascii_to_ebcdic(), []}
       end
 
-    #Logger.debug("[#{inspect(str_mti)}|#{inspect(str_bp)}|#{inspect(str_bs)}|#{inspect(value)}]")
+    # Logger.debug("[#{inspect(str_mti)}|#{inspect(str_bp)}|#{inspect(str_bs)}|#{inspect(value)}]")
 
     case str_bs do
       "0000000000000000" ->
@@ -331,13 +350,16 @@ defmodule Ale8583 do
   """
   def list_to_iso({str_bit_map, list, type, str_header_prosa}, iso) do
     list_bit_map = Ale8583.BitMap.get_list_bit_map(str_bit_map)
-    #Logger.debug("BitMap: #{str_bit_map}  List:#{inspect list_bit_map}")
+    # Logger.debug("BitMap: #{str_bit_map}  List:#{inspect list_bit_map}")
     cond do
       type in [:prosa, :mc] ->
         iso =
           if type == :prosa do
             {:iso, list_iso, {bit_map_p, bit_map_s, flag_bm_sec, iso_conf}, status} = iso
-            list_iso = List.keyreplace(list_iso, :header_prosa, 0, {:header_prosa, str_header_prosa})
+
+            list_iso =
+              List.keyreplace(list_iso, :header_prosa, 0, {:header_prosa, str_header_prosa})
+
             {:iso, list_iso, {bit_map_p, bit_map_s, flag_bm_sec, iso_conf}, status}
           else
             iso
@@ -443,7 +465,8 @@ defmodule Ale8583 do
   defp new(:conf, {mti, list_conf}, headerProsa) do
     case mti in @msgs do
       true ->
-        {:iso, [mti: mti, bp: "0000000000000000", bs: "0000000000000000", header_prosa: headerProsa],
+        {:iso,
+         [mti: mti, bp: "0000000000000000", bs: "0000000000000000", header_prosa: headerProsa],
          {Ale8583.BitMap.newBitMapP(), Ale8583.BitMap.newBitMapS(), :flag_bm_sec_no, list_conf},
          {:ok, "OK"}}
 
@@ -463,9 +486,15 @@ defmodule Ale8583 do
       {:ok, list_conf} ->
         case mti in @msgs do
           true ->
-            {:iso, [mti: mti, bp: "0000000000000000", bs: "0000000000000000", header_prosa: headerProsa],
-             {Ale8583.BitMap.newBitMapP(), Ale8583.BitMap.newBitMapS(), :flag_bm_sec_no, list_conf},
-             {:ok, "OK"}}
+            {:iso,
+             [
+               mti: mti,
+               bp: "0000000000000000",
+               bs: "0000000000000000",
+               header_prosa: headerProsa
+             ],
+             {Ale8583.BitMap.newBitMapP(), Ale8583.BitMap.newBitMapS(), :flag_bm_sec_no,
+              list_conf}, {:ok, "OK"}}
 
           false ->
             {:error, [], {}, "Error MTI no valid:  #{mti}, Those mtis ara valid: #{@msgs}."}
@@ -484,24 +513,32 @@ defmodule Ale8583 do
   """
   defp do_printAll([head | tail], list_iso_conf, buffer) do
     {key, value} = head
-    # Logger.warn "key #{inspect key}, value #{inspect value} isoList #{inspect list_iso_conf}"
+    # Logger.warn("key #{inspect(key)}, value #{inspect(value)}")
+
     buffer =
       if key in [:mti, :bp, :bs, :header_prosa] do
-          buffer
+        buffer
         # buffer = buffer <> "#{key}<#{value}>\n"
       else
         {_, map} = List.keyfind(list_iso_conf, key, 0)
+
         case map.var_length do
           0 ->
             buffer <> " #{key}<#{value}>\n"
+
           2 ->
-            buffer <> " #{key}<#{String.slice(value, 0, 2)}>\n"
+            # buffer <> " #{key}<#{String.slice(value, 0, 2)}>\n"
             buffer <> " #{key}<#{String.slice(value, 2..(map.length + 1))}>\n"
+
+          # buffer <> " #{key}<#{String.slice(value, 0..(map.length + 1))}>\n"
+
           3 ->
-            buffer <> " #{key}<#{String.slice(value, 0, 3)}>\n"
+            # buffer <> " #{key}<#{String.slice(value, 0, 3)}>\n"
             buffer <> " #{key}<#{String.slice(value, 3..(map.length + 1))}>\n"
+            # buffer <> " #{key}<#{String.slice(value, 0..(map.length + 1))}>\n"
         end
       end
+
     do_printAll(tail, list_iso_conf, buffer)
   end
 
@@ -512,7 +549,7 @@ defmodule Ale8583 do
 
   """
   defp do_printAll([], _, buffer) do
-    Logger.error(buffer)
+    Logger.info(buffer)
   end
 
   @doc """
@@ -535,7 +572,6 @@ defmodule Ale8583 do
     do_getAppendISO(list_iso, list_iso_conf, [])
   end
 
-
   @doc """
   Private function  for logic from getAppendISO() funtion .
 
@@ -555,17 +591,20 @@ defmodule Ale8583 do
         case map.code do
           :exc_mc_48 ->
             value |> String.to_charlist() |> Ale8583.Convert.ascii_to_exc_mc_48()
+
           :ebcdic ->
             value |> String.to_charlist() |> Ale8583.Convert.ascii_to_ebcdic()
+
           :ascii ->
             String.to_charlist(value)
+
           :bin ->
             value |> String.to_charlist() |> Ale8583.Convert.ascii_to_bin()
         end
+
       do_getAppendISO(tail, list_iso_conf, acc ++ value)
     end
   end
-
 
   @doc """
   Private function  to  catch accoumulator from getAppendISO() funtion .
@@ -577,68 +616,80 @@ defmodule Ale8583 do
     acc
   end
 
-
   @doc """
   Private function .
 
   Returns `iso`
 
   """
-  defp sec_add_field(key, atom_key, value, list_bit_map_p, list_bit_map_s, list_iso, flag_bm_sec, list_iso_conf) do
+  defp sec_add_field(
+         key,
+         atom_key,
+         value,
+         list_bit_map_p,
+         list_bit_map_s,
+         list_iso,
+         flag_bm_sec,
+         list_iso_conf
+       ) do
     case List.keymember?(list_iso, atom_key, 0) do
       true ->
         {:iso, list_iso |> List.keyreplace(atom_key, 0, {atom_key, value}) |> Enum.sort(),
          {list_bit_map_p, list_bit_map_s, flag_bm_sec, list_iso_conf}, {:ok, "OK UPDATE"}}
+
       false ->
         list_iso = (list_iso ++ ["c#{key}": value]) |> Enum.sort()
+
         cond do
           key in 1..64 ->
             list_bit_map_p = Ale8583.BitMap.modifyInsert(list_bit_map_p, atom_key)
+
             list_iso =
               List.keyreplace(
-              list_iso,
-              :bp,
-              0,
-              {:bp, Ale8583.BitMap.getBitMapHex(list_bit_map_p)}
-            )
+                list_iso,
+                :bp,
+                0,
+                {:bp, Ale8583.BitMap.getBitMapHex(list_bit_map_p)}
+              )
+
             {:iso, list_iso, {list_bit_map_p, list_bit_map_s, flag_bm_sec, list_iso_conf},
-            {:ok,
-              "OK INSERT AND PRIMARY BITMAP UPDATE,#{atom_key}-#{key}: value #{value}"}}
+             {:ok, "OK INSERT AND PRIMARY BITMAP UPDATE,#{atom_key}-#{key}: value #{value}"}}
 
           key in 65..128 ->
             {list_bit_map_p, flag_bm_sec, list_iso} =
-            if flag_bm_sec == :flag_bm_sec_no do
-              #Logger.debug("flag_bit_map_sec #{flag_bm_sec} ")
-              list_bit_map_p_temp = Ale8583.BitMap.modifyInsert(list_bit_map_p, :c1)
-              list_iso_temp = List.keyreplace(
-                  list_iso,
-                  :bp,
-                  0,
-                  {:bp, Ale8583.BitMap.getBitMapHex(list_bit_map_p_temp)}
-                )
-              {list_bit_map_p_temp,
-                :flag_bm_sec_yes,
-                list_iso_temp}
-            else
-              {list_bit_map_p, flag_bm_sec, list_iso}
-            end
+              if flag_bm_sec == :flag_bm_sec_no do
+                # Logger.debug("flag_bit_map_sec #{flag_bm_sec} ")
+                list_bit_map_p_temp = Ale8583.BitMap.modifyInsert(list_bit_map_p, :c1)
 
-            #Logger.debug("List bit mapP : #{inspect list_bit_map_p}\n Flag bm Sec:  #{flag_bm_sec}, \n #{inspect list_iso}")
+                list_iso_temp =
+                  List.keyreplace(
+                    list_iso,
+                    :bp,
+                    0,
+                    {:bp, Ale8583.BitMap.getBitMapHex(list_bit_map_p_temp)}
+                  )
+
+                {list_bit_map_p_temp, :flag_bm_sec_yes, list_iso_temp}
+              else
+                {list_bit_map_p, flag_bm_sec, list_iso}
+              end
+
+            # Logger.debug("List bit mapP : #{inspect list_bit_map_p}\n Flag bm Sec:  #{flag_bm_sec}, \n #{inspect list_iso}")
             list_bit_map_s = Ale8583.BitMap.modifyInsert(list_bit_map_s, atom_key)
+
             {:iso,
-              List.keyreplace(
-                list_iso,
-                :bs,
-                0,
-                {:bs, Ale8583.BitMap.getBitMapHex(list_bit_map_s)}
-              ), {list_bit_map_p, list_bit_map_s, flag_bm_sec, list_iso_conf},
-              {:ok,
-                "OK INSERT AND SECUNDARY BITMAP UPDATE,#{atom_key}-#{key}: value #{value}"}}
+             List.keyreplace(
+               list_iso,
+               :bs,
+               0,
+               {:bs, Ale8583.BitMap.getBitMapHex(list_bit_map_s)}
+             ), {list_bit_map_p, list_bit_map_s, flag_bm_sec, list_iso_conf},
+             {:ok, "OK INSERT AND SECUNDARY BITMAP UPDATE,#{atom_key}-#{key}: value #{value}"}}
 
           true ->
             {:iso, list_iso, {list_bit_map_p, list_bit_map_s, flag_bm_sec, list_iso_conf},
-            {:ok, "FIELD NOT SUPPORTED, FIELD SHOULD BE BETWEEN 2..128."}}
+             {:ok, "FIELD NOT SUPPORTED, FIELD SHOULD BE BETWEEN 2..128."}}
         end
-      end
+    end
   end
 end
